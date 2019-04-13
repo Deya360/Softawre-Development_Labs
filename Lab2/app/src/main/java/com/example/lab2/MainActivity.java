8a1ab3eb68617e18d9074feac755df10032cab5d
Deya360 <32934092+Deya360@users.noreply.github.com>
2019-04-14 00:53:58 +0300

    Fine Tunning

---
+++ Lab2/app/src/main/java/com/example/lab2/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.lab2;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+    ListView myLv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        overridePendingTransition(R.anim.slide_in,R.anim.slide_out);
+        setContentView(R.layout.activity_main);
+
+        String tag = getResources().getString(R.string.listFragTag);
+
+        FragmentManager manager = getSupportFragmentManager();
+        Fragment listFrag = manager.findFragmentByTag(tag);
+        if (listFrag == null) {
+            listFrag = new ListFrag();
+            manager.beginTransaction()
+                    .setCustomAnimations(R.anim.slide_up, 0, 0, R.anim.slide_down)
+                    .replace(R.id.MainLayout,listFrag,tag)
+                    .commit();
+        }
+    }
+
+}
+
